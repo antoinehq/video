1a3563364b638fe6c54dd98858df520dc6d0e44b
LoicRenault <renault.loic.acm@gmail.com>
2020-04-21 12:19:28 +0200

    la real

---
+++ src/main/java/video/model/Realisation.java
@@ -0,0 +1,49 @@
+package video.model;
+
+public class Realisation {
+	// attributs
+	private RealisationKey id;
+
+	// constructors
+	public Realisation(RealisationKey id) {
+		this.id = id;
+	}
+
+	public Realisation() {
+	}
+
+	// getters and setters
+	public RealisationKey getId() {
+		return id;
+	}
+
+	public void setId(RealisationKey id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Realisation other = (Realisation) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
