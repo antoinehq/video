2ff322ef4103e3a1ed89df938b0d7198e546f56e
christopher <c.merlin@net.estia.fr>
2020-04-21 17:58:10 +0200

    DaoFilm

---
+++ src/main/java/video/dao/DaoFilmImpl.java
@@ -0,0 +1,136 @@
+package video.dao;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLType;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import video.context.Context;
+import video.model.Film;
+
+class DaoFilmImpl implements DaoFilm {
+	
+	@Override
+	public void insert(Film obj) {
+		try (PreparedStatement ps = Context.getInstance().getConnection().prepareStatement(
+				"insert into film(id_film,titre,date_sortie) values(nextval('seq_film'),?,?)",
+				PreparedStatement.RETURN_GENERATED_KEYS)) {
+			ps.setString(1, obj.getTitre());
+			if (obj.getDate_sortie() != null) {
+				ps.setDate(2, new Date(obj.getDate_sortie().getTime()));
+			} else {
+				ps.setNull(2, Types.DATE);
+			}
+
+			ps.executeUpdate();
+			Context.getInstance().getConnection().commit();
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) {
+				obj.setId_film(rs.getInt(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				Context.getInstance().getConnection().rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		Context.destroy();
+	}
+
+	@Override
+	public Film update(Film obj) {
+		try (PreparedStatement ps = Context.getInstance().getConnection()
+				.prepareStatement("update Film set titre=?, date_sortie=? where id_film=?")) {
+			ps.setString(1, obj.getTitre());
+			if (obj.getDate_sortie() != null) {
+				ps.setDate(2, new Date(obj.getDate_sortie().getTime()));
+			} else {
+				ps.setNull(2, Types.DATE);
+			}
+			ps.setInt(3, obj.getId_film());
+			ps.executeUpdate();
+			Context.getInstance().getConnection().commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				Context.getInstance().getConnection().rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		Context.destroy();
+		return obj;
+	}
+
+	@Override
+	public void delete(Film obj) {
+		deleteById(obj.getId_film());
+	}
+
+	@Override
+	public void deleteById(Integer key) {
+		try (PreparedStatement ps = Context.getInstance().getConnection()
+				.prepareStatement("delete from film where id=?")) {
+			ps.setInt(1, key);
+			ps.executeUpdate();
+			Context.getInstance().getConnection().commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				Context.getInstance().getConnection().rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		Context.destroy();
+	}
+
+	@Override
+	public Optional<Film> findByKey(Integer key) {
+		Film film = null;
+		try (PreparedStatement ps = Context.getInstance().getConnection()
+				.prepareStatement("select * from film where id_film=?")) {
+			ps.setInt(1, key);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				film = new Film(rs.getInt("id_film"), rs.getString("titre"));
+				film.setDate_sortie(rs.getDate("date_sortie"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		Context.destroy();
+		return Optional.ofNullable(film);
+	}
+
+	@Override
+	public List<Film> findAll() {
+		List<Film> films = new ArrayList<Film>();
+		try (Statement st = Context.getInstance().getConnection().createStatement()) {
+			ResultSet rs = st.executeQuery("select * from film");
+			while (rs.next()) {
+				films.add(new Film(rs.getInt("id_film"), rs.getString("titre"), rs.getDate("date_sortie")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		Context.destroy();
+		return films;
+	}
+
+	@Override
+	public List<Film> findByArticle(String titre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
